aae94c666007b5288dd30376444d6830c65d7f8c
Gladden <mr.gladdenpravin@gmail.com>
2025-10-23 21:20:14 +0530

    Time: 1 ms (35.54%) | Memory: 44.6 MB (78.79%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        
+        for (int i = 0; i < n; i++) {
+            // Reverse the row
+            for (int j = 0; j < n / 2; j++) {
+                int temp = image[i][j];
+                image[i][j] = image[i][n - 1 - j];
+                image[i][n - 1 - j] = temp;
+            }
+            
+            // Invert using XOR with 1
+            for (int j = 0; j < n; j++) {
+                image[i][j] ^= 1;
+            }
+        }
+        
+        return image;
+    }
+}
