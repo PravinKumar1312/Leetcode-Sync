873cbdaa80c3868671d55f70c6100e5c50e9ab6f
Gladden <mr.gladdenpravin@gmail.com>
2025-11-12 18:08:41 +0530

    Time: 46 ms (92.12%) | Memory: 55 MB (26.41%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    // Wait for both promises to resolve and get their values
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    
+    // Return the sum of the two values
+    return value1 + value2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
