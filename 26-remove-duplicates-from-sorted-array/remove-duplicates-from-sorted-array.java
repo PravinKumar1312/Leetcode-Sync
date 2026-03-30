beaa0c94aaeae95a17a711e986d5c7cf92998916
Gladden <mr.gladdenpravin@gmail.com>
2026-03-30 16:03:42 +0530

    Time: 1 ms (78.21%) | Memory: 46.6 MB (87.88%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,13 +1,39 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
-        int k = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i-1]) {
-                nums[k] = nums[i];
-                k++;
+    public int removeDuplicates(int[] a) {
+        int n = a.length;
+        int i = 0;
+        if(n==0)
+            return 0;
+        for(int j=1;j<n;j++){
+            if(a[i]!=a[j]){
+                i++;
+                a[i]=a[j];
             }
         }
-        return k;
+        return i+1;
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        // if (nums.length == 0) return 0;
+        // int k = 1;
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] != nums[i-1]) {
+        //         nums[k] = nums[i];
+        //         k++;
+        //     }
+        // }
+        // return k;
     }
 }
