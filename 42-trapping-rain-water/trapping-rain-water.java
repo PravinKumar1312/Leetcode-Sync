16af21fb03fbae7fa0855f56866296337ca50c10
Gladden <mr.gladdenpravin@gmail.com>
2026-03-31 13:40:51 +0530

    Time: 1 ms (59.14%) | Memory: 48.7 MB (26.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,25 @@
 class Solution {
-    public int trap(int[] height) {
-        int len = height.length;
-        int[] left = new int[len];
-        left[0] = height[0];
-        for (int i = 1; i < len; i++) {
-            left[i] = Math.max(height[i], left[i - 1]);
+    public int trap(int[] b) {
+        int n = b.length;
+        if(n==1 || n ==2)
+            return 0;
+        int[] left_max = new int[n];
+        left_max[0] = b[0];
+        for(int i = 1; i<n;i++){
+            left_max[i]=Math.max(b[i],left_max[i-1]);
         }
 
-        int[] right = new int[len];
-        right[len - 1] = height[len - 1];
-        for (int i = len - 2; i >= 0; i--) {
-            right[i] = Math.max(height[i], right[i + 1]);
+        int[] right_max=new int[n];
+        right_max[n-1]=b[n-1];
+        for(int i = n-2; i>=0;i--){
+            right_max[i] = Math.max(b[i],right_max[i+1]);
         }
-        int water = 0;
-        for (int i = 0; i < len; i++) {
-            water += Math.min(left[i], right[i]) - height[i];
+        
+        int sum = 0;
+        for(int i = 0; i<n;i++){
+            int WL=Math.min(left_max[i],right_max[i]);
+            sum += WL-b[i];
         }
-        return water;
+        return sum;
     }
 }
