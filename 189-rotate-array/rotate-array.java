8241e687abe5cd2c3eae1b59451dd0f56beb67c4
Gladden <mr.gladdenpravin@gmail.com>
2026-04-01 17:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 61.6 MB (27.31%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] a, int k) {
+        int n = a.length;
+        k = k%n;
+        reverse(a,0,n-1);
+        reverse(a,0,k-1);
+        reverse(a,k,n-1);
+    }
+
+    public static void reverse(int[]a, int start, int end){
+        while(start<end){
+            int temp = a[start];
+            a[start]=a[end];
+            a[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
