ae019fce98c65a09c1a864a21af49d6fa50c3faa
Gladden <mr.gladdenpravin@gmail.com>
2026-04-03 17:32:06 +0530

    Time: 2 ms (36.54%) | Memory: 42.7 MB (78.35%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+
+        while(n>1){
+            Arrays.sort(stones,0,n);
+
+            int x = stones[n-1];
+            int y = stones[n-2];
+            if(x==y){
+                n-=2;
+            }
+            else{
+                stones[n-2] = x-y;
+                n-=1;
+            }
+        }
+        return n == 1? stones[0] : 0;
+    }
+}
